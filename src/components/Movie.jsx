b70f0acbcd46c1f0b07fc41884ef0c372fd533c3
yno1109 <yno1109>
2023-03-08 17:28:06 +0900

    [add] search function

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 
+// FIXME: 깨진 디자인 다시 손보기
 const Movie = ({ item }) => {
-  const MAX_LENGTH = 55;
+  const MAX_LENGTH = 20;
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col w-96 px-16 pt-16 rounded-lg bg-gray-300 shadow-lg shadow-gray-900">
+      <div className="flex flex-col w-72 px-16 pt-16 rounded-lg bg-gray-300 shadow-lg shadow-gray-900">
         <Link to={`/detail/${item.id}`}>
           <img
-            className="w-full rounded-sm shadow-lg shadow-gray-600"
+            className="w-60 rounded-sm shadow-lg shadow-gray-600"
             src={item.medium_cover_image}
             alt={item.title}
           />
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -11,16 +11,41 @@ const Home = () => {
   const location = useLocation();
   const [isLoggined, setIsLoggined] = useState(location?.state);
   const [isLoading, setIsLoading] = useState(true);
+  const [keyword, setkeyword] = useState("");
+  const [error, setError] = useState("");
   const [movies, setMovies] = useState([]);
 
   const callAPI = async () => {
     const json = await (
       await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
       )
     ).json();
     setIsLoading(false);
     setMovies(json.data.movies);
+  };
+
+  const changeKeyword = (event) => {
+    setkeyword(event.target.value);
+  };
+
+  const searchMovie = (event) => {
+    event.preventDefault();
+    fetch(`https://yts.mx/api/v2/list_movies.json?query_term=${keyword}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error();
+        }
+        response.json();
+      })
+      .then((json) => {
+        setIsLoading(false);
+        setMovies(json.data.movies);
+      })
+      .catch(() => {
+        setMovies([]);
+        setError(`${keyword}라는 항목을 찾을 수 없습니다.`);
+      });
   };
 
   useEffect(() => {
@@ -29,7 +54,8 @@ const Home = () => {
 
   return (
     // TODO: 우측 상단에 설정 옵션 만들고 마이페이지 만들기
-    // TODO: 원하는 영화 검색과 장르나 최신영화 검색할 수 있게 input 만들기
+    // TODO: 원하는 영화 검색 부드럽게 만들고 장르나 최신영화 검색할 수 있게 input 만들기
+    // FIXME: search bar 디자인 수정
     <div className="h-screen flex flex-col relative">
       <Header isLoggined={isLoggined} />
       <section className="flex flex-1 justify-center items-center p-36 mt-24 bg-slate-500">
@@ -37,15 +63,18 @@ const Home = () => {
           <Loading />
         ) : (
           <div>
-            <div className="flex justify-around items-center mb-24 bg-slate-300 px-8 py-10 rounded-2xl shadow-md shadow-gray-900">
+            <div className="flex justify-around items-center mb-24 bg-slate-300 px-8 py-10 rounded-2xl shadow-md shadow-gray-900 w-full">
               <form>
                 <div className="flex items-center w-96 rounded-full px-4 py-2 bg-white">
                   <input
+                    onChange={changeKeyword}
+                    value={keyword}
                     type="text"
                     placeholder="Write a Title!"
                     className="outline-none w-full text-slate-300"
+                    required
                   />
-                  <button>
+                  <button onClick={searchMovie}>
                     <FontAwesomeIcon
                       icon={faMagnifyingGlass}
                       size="xl"
@@ -61,11 +90,15 @@ const Home = () => {
                 <option>평점높은순</option>
               </select>
             </div>
-            <div className="grid grid-cols-3 gap-x-56 gap-y-28">
-              {movies.map((movie) => (
-                <Movie key={movie.id} item={movie} />
-              ))}
-            </div>
+            {movies.length !== 0 ? (
+              <div className="grid grid-cols-3 gap-x-56 gap-y-28">
+                {movies.map((movie) => (
+                  <Movie key={movie.id} item={movie} />
+                ))}
+              </div>
+            ) : (
+              <div className="text-center">{error}</div>
+            )}
           </div>
         )}
       </section>
